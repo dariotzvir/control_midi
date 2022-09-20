252cc0cc33c5e9ce266754dde7314b9afdd7fc9a
Darío Tzvir <dariotzvir@gmail.com>
2022-09-19 23:17:55 -0300

    .

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <QApplication>
 #include <QPushButton>
+#include <nlohmann/json.hpp>
+#include <iostream>
+using json = nlohmann::json;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+    std::ifstream jorge("config/mkII_data.json", std::ifstream::in);
+    json data = json::parse(jorge);
+    std::cout << data << endl;
     QApplication a(argc, argv);
     QPushButton button("Hello world!", nullptr);
     button.resize(200, 100);
